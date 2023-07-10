f95c9b3a897ba9532351302f899fc4972ea7bd96
alexkrowg <alexkrowg@gmail.com>
2023-07-10 17:54:52 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading="Loading" persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading="Loading" persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
   </React.StrictMode>
 );
 
